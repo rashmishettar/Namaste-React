c8524c8b1f3d1d7ec299a8982adbb4bf2731cf51
Yogendra G R <yogendra@sellergeni.com>
2024-07-04 18:03:27 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1", { id: "id", xyz: "xyz" }, "Hello world from React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
